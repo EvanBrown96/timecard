2f703af9d44b1f3b36293661b468f4fd090a7b69
EvanBrown96 <evan.br96@yahoo.com>
2019-08-17 22:52:30 -0500

    started navbar

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,74 @@
 import Browser
 import Html exposing (..)
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Bootstrap.Card as Card
+import Bootstrap.Navbar as Nav
+import Platform.Cmd exposing (Cmd)
+import Platform.Sub as Sub exposing (Sub)
+import Html.Attributes as Attr
 
 -- main definition
 main =
-  Browser.sandbox
-    { init = ()
+  Browser.element
+    { init = init
     , view = view
     , update = update
+    , subscriptions = subscriptions
     }
 
 -- model is currently empty
 type alias Model =
-  ()
+  Nav.State
 
 -- no message types yet
-type alias Msg =
-  ()
+type Msg = NavUpdate Nav.State
 
--- update does nothing currently
-update : Msg -> Model -> Model
-update _ _ =
-  ()
+-- init creates initial state for the navbar
+init : () -> (Model, Cmd Msg)
+init _ =
+  Nav.initialState NavUpdate
+
+-- no subscriptions currently
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
+
+-- update function
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NavUpdate state ->
+      (state, Cmd.none)
 
 view : Model -> Html Msg
-view _ =
-  text "nothing here yet"
+view model =
+  Grid.container
+    []
+    [ CDN.stylesheet
+    , Grid.row
+        []
+        [ Grid.col
+            []
+              [ Nav.config NavUpdate
+                  |> Nav.secondary
+                  |> Nav.items
+                      [ Nav.itemLink
+                          []
+                          [ a
+                              [ Attr.href "http://test1" ]
+                              [ text "Test 1" ]
+                          ]
+                      , Nav.itemLinkActive
+                          []
+                          [ a
+                              [ Attr.href "http://current" ]
+                              [ text "Current" ]
+                          ]
+                      ]
+                  |> Nav.view model
+              ]
+        ]
+    ]
